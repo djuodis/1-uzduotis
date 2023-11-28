5c4404a0accdfa227befd10f19c43296149787a7
djuodis <deividas73245@gmail.com>
2023-11-28 15:39:33 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+function bigVardas() {
+    let vardas = document.querySelector('h1')
+    let body = document.querySelector('body')
+    vardas.style.fontSize = '96px'
+    vardas.style.color = 'red'
+    body.style.display = 'flex'
+    body.style.alignItems = 'center'
+    body.style.justifyContent = 'center'
+  }
